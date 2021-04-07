3d66c1f8d1e6e2b8d81f38a8f9f4b52a7bc20970
Seth Harmer <>
2021-04-07 05:24:47 +0000

    Solution.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    greeting = input("Greetings! How are you doing?")
+    tell = input("Oh, how interesting. Tell me more!")
+    print("Thanks for sharing!")
+    
 
 if __name__ == '__main__':
     main()
